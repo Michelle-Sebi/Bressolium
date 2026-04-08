d0688e302393a707652dda905de68f04442f2c62
barbarabur <correudebarbara@gmail.com>
2026-04-08 19:39:51 +0200

    feat/t14 Seeder modified

---
+++ Bressolium/backend/BressoliumProject/database/seeders/TechnologiesBaseSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class TechnologiesBaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $wheelId = Str::uuid()->toString();
+        DB::table('technologies')->insert([
+            'id' => $wheelId,
+            'name' => 'Wheel',
+            'prerequisite_id' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        $mathId = Str::uuid()->toString();
+        DB::table('technologies')->insert([
+            'id' => $mathId,
+            'name' => 'Mathematics',
+            'prerequisite_id' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        
+        // Also add Cart so old stuff doesn't completely break, even though not explicitly asserted here.
+        $cartId = Str::uuid()->toString();
+        DB::table('inventions')->insert([
+            'id' => $cartId,
+            'name' => 'Cart',
+            'technology_id' => $wheelId,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
